4c5ea843f6e28af9e5c210b9f891a4e07f0dcf9f
Nur Azizah <nahanizalba9@gmail.com>
2022-02-11 21:47:35 +0700

    tablet base

--- pages/about.js
+++ pages/about.js
@@ -15,7 +15,7 @@ const AboutPage = () => {
     }, []);
 
     return (
-        <div className='w-screen h-screen pb-20 bg-[#F6F6F6]'>
+        <div className='w-screen h-screen  overflow-y-auto lg:overflow-y-hidden xl:overflow-y-auto pb-10 lg:pb-20 bg-[#F6F6F6]'>
             <div className={'flex justify-end pad-0'}>
                 {/* <Link href={`/under_construction`} passHref> */}
                     <div className={'w-10 h-10 flex justify-center items-center cursor-pointer bg-[#E6E9ED]'}onClick={() => {
@@ -29,20 +29,20 @@ const AboutPage = () => {
             <div className={'flex flex-row min-h-full min-w-full px-10 text-white'}>
                 <Fade duration={1600} bottom>
                     <div className={'grid grid-cols-12'}>
-                        <div className={'col-span-3 bg-[#707070]'}>
-                            <div className={'text-center bg-[#E6E9ED] h-3/5'}>
-                                <Image preview={false} className="w-80" style={{marginTop:'50%'}} src={'/assets/logo/logo-for-light-background.png'} />
+                        <div className={'col-span-12 md:col-span-5 lg:col-span-3 xl:col-span-3 bg-[#707070]'}>
+                            <div className={'text-center bg-[#E6E9ED] imageLogo h-3/5'}>
+                                <Image preview={false} className="w-80" src={'/assets/logo/logo-for-light-background.png'} />
                             </div>
                             <Fade when={transition} duration={1600} right>
-                                <div className={'bg-no-repeat bg-left-bottom  p-20 h-2/5 bg-[#FE6601]'}  style={{backgroundImage:"url('/assets/accents/finger-print.png')", backgroundSize:220, backgroundPositionX:'-100px', backgroundPositionY:150}}>
-                                    <p className="font-bold text-xl">The Most Advance <br />
+                                <div className={'bg-no-repeat bg-left-bottom  px-10 py-4 md:p-10 xl:p-20 h-2/5 bg-[#FE6601]'}  style={{backgroundImage:"url('/assets/accents/finger-print.png')", backgroundSize:220, backgroundPositionX:'-100px', backgroundPositionY:150}}>
+                                    <p className="font-bold text-lg xl:text-xl">The Most Advance <br />
                                     <span className='font-light'> End-To-End</span><br />
                                         Digital Biometrics <br />
                                         Solution Provider</p>
                                 </div>
                             </Fade>
                         </div>
-                        <div className={'col-span-4'}>
+                        <div className={'col-span-12 md:col-span-7 lg:col-span-4 xl:col-span-4'}>
                             <div className={'h-3/5 bg-[#D8D8D8]'}>
                                 <Slide when={transition} duration={1600} left>
                                     <div className=" bg-[#D8D8D8] h-full">
@@ -50,27 +50,27 @@ const AboutPage = () => {
                                     </div>
                                 </Slide>
                             </div>
-                            <div className={'h-2/5 px-20 py-14 bg-[#FFB280]'}>
-                                <p className={'text-base font-light tracking-widest'}>CALL US <br />
+                            <div className={'h-2/5 px-10 xl:px-20 py-6 xl:py-14 bg-[#FFB280]'}>
+                                <p className={'text-sm xl:text-base font-light tracking-widest'}>CALL US <br />
                                 <Fade when={transition} duration={1600} bottom>
                                     <Fade when={transition} duration={1600} right>
-                                        <div className='font-normal tracking-normal text-2xl'>(021) 22902348</div>
+                                        <div className='font-normal tracking-normal text-xl xl:text-2xl'>(021) 22902348</div>
                                     </Fade>
                                 </Fade>
                                 <br />
                                <p className={'-my-6'}> Mon - Fri (8.30 - 17.30)</p><br />
                                 <p className={'mt-8 -mb-px'}>EMAIL <br /></p>
-                                <span className='font-normal tracking-normal text-2xl'>info@withtrustlink.com</span></p>
+                                <span className='font-normal tracking-normal text-xl xl:text-2xl'>info@withtrustlink.com</span></p>
                             </div>
                         </div>
-                        <div className={'bg-[#707070] col-span-5 '}>
+                        <div className={'bg-[#707070] col-span-12  md:col-span-12 lg:col-span-5 xl:col-span-5 '}>
                             <Fade when={transition} duration={1600} left>
-                                <div className={'p-16 h-full bg-[#04204D]'}>
-                                    <p className='text-5xl font-bold mt-28'>About.</p>
-                                    <p className='text-xl mt-20'>
+                                <div className={'p-10 lg:p-12 xl:p-16 h-full bg-[#04204D]'}>
+                                    <p className='text-3xl xl:text-5xl font-bold mt-9 md:mt-12 lg:20 xl:mt-28'>About.</p>
+                                    <p className='text-lg xl:text-xl mt-8 md:mt-10 lg:mt-14 xl:mt-20'>
                                         The Only Open Finance With Biometrics Ekyc Verification
                                     </p>
-                                    <p className='text-gray-400 mt-6 text-lg'>
+                                    <p className='text-gray-400 mt-6 text base xl:text-lg'>
                                         We are the only company that provide the most advanced identity verification against Indonesian national database as the official Dukcapil partner in Platform Bersama that combine it with open finance facilitator for a seamless, simpler, and most secure financial data integration
                                     </p>
                                 </div>
