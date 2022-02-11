d83a86ec8642c9155e2d398e0a8adcd236db9748
Tsabit Ghazwan <tsabitg23@gmail.com>
2022-02-11 13:23:56 +0700

    fix: progress home page

--- pages/under_construction.js
+++ pages/under_construction.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Carousel, Image} from 'antd';
 import { Image } from 'antd';
 import Link from "next/link";
 
@@ -11,6 +12,21 @@ const UnderConstruction = () => {
         {
             label: "contact us",
             link: 'contact_us'
+        }
+    ];
+
+    const text = [
+        {
+            label: "Coming Soon"
+        },
+        {
+            label: "Analytic Platforms"
+        },
+        {
+            label: "Take the lead"
+        },
+        {
+            label: "The future"
         }
     ];
 
@@ -27,14 +43,38 @@ const UnderConstruction = () => {
                 )
             })}
         </div>
-    )
+    );
+
+
     return (
         <div className={"w-screen h-screen under-construction"}>
             <div style={{paddingLeft:"4vw", paddingRight:'4vw', paddingTop:'8vh', paddingBottom:'8vh'}}>
                 <div className={'container-1'}>
                     <Image className={"w-3/5 sm:w-3/5 md:w-3/5 xl:w-3/6"} style={{marginLeft:-15}} preview={false} src={'assets/logo/logo-for-light-background.png'} alt={"Trustlink"}/>
-                    <div>
-                        <span className={'we-are-text'}>We Are</span>
+                    <div className={"grid grid-cols-5 gap-4"}>
+                        <span className={'col-span-5 xl:col-span-1 we-are-text'}>We Are</span>
+                        <div className={'col-span-5 xl:col-span-4'}>
+                            <div className={"slider-container"}>
+                                <Carousel
+                                    dotPosition={'right'}
+                                    dots={false}
+                                    autoplay={true}
+                                    speed={1000}
+                                    autoplaySpeed={4000}
+                                >
+                                    {
+                                        text.map(prop => {
+                                            return (
+                                                <div key={prop.label}>
+                                                    <span className={'text-slider'}>{prop.label}</span>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </Carousel>
+                            </div>
+
+                        </div>
                     </div>
                     <RenderMenu />
                 </div>
