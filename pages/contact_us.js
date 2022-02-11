a3a63b803ea6eaa9d56f324549a4f4b6124583c8
RiziqSusanto <riziq.susanto@gmail.com>
2022-02-11 16:17:25 +0700

    fix: responsive contact us

--- pages/contact_us.js
+++ pages/contact_us.js
@@ -13,7 +13,7 @@ const ContactPage = () => {
         })
     }
     return (
-        <div className={'h-screen pb-20'}>
+        <div className={'h-screen pb-20 overflow-y-auto lg:overflow-y-hidden'}>
             <div className={'flex justify-end pad-0'}>
                 <Link href={`/under_construction`} passHref>
                     <div className={'w-10 h-10 flex justify-center items-center cursor-pointer'} style={{backgroundColor: '#e6e9ed'}}>
@@ -22,62 +22,64 @@ const ContactPage = () => {
                 </Link>
             </div>
 
-            <div className={'flex flex-row min-h-full px-10'}>
-                <div className={'w-3/5 flex items-center relative'} style={{backgroundColor: '#FE6601'}}>
-                    <div className={'text-white pl-14'}>
-                        <p className={'text-5xl font-bold'}>Be the first <br/>to hear from us</p>
-                        <p className={'text-sm w-72 pt-0'}>Sign Up to get notified whe our website <br/> is launch. We have some great stuff <br/>comin to you. Stay tuned!</p>
+            <div className={'flex flex-col lg:flex-row min-h-full px-3 lg:px-10'}>
+                <div className={'w-full lg:w-3/5 flex items-center relative justify-center lg:justify-start'} style={{backgroundColor: '#FE6601'}}>
+                    <div className={'text-white pl-2 lg:pl-14'}>
+                        <p className={'text-4xl lg:text-5xl font-bold mt-10 text-center lg:text-left lg:mt-0'}>Be the first <br/>to hear from us</p>
+                        <p className={'text-sm lg:w-72 pt-0 text-center lg:text-left'}>Sign Up to get notified whe our website <br/> is launch. We have some great stuff <br/>comin to you. Stay tuned!</p>
 
                         <Form
                             layout={"vertical"}
-                            className={'pt-20'}
+                            className={'pt-10 lg:pt-20'}
                         >
                             <Row gutter={16}>
-                                <Col span={12}>
+                                <Col xs={{span: 22}} sm={{span: 12}} md={{span: 12}} className={'text-center'}>
                                     <Form.Item>
                                         <Input className={'placeholder-white border-transparent	text-white w-52 h-10'} style={{backgroundColor: '#FE944D'}} placeholder="Full Name"/>
                                     </Form.Item>
                                 </Col>
-                                <Col span={12}>
+                                <Col xs={{span: 22}} sm={{span: 12}} md={{span: 12}} className={'text-center'}>
                                     <Form.Item>
                                         <Input className={'placeholder-white border-transparent	w-52 text-white h-10'} style={{backgroundColor: '#FE944D'}} placeholder="Email"/>
                                     </Form.Item>
                                 </Col>
                             </Row>
                         </Form>
-                        <div className={'pt-20 flex flex-row'}>
+                        <div className={'pt-20 flex flex-row crop-div'}>
+                        </div>
+                        <div className={'crop-div'}>
+                            <Image src={'/assets/accents/finger-print.png'} preview={false} className={'crop-img'}/>
                         </div>
                     </div>
-                    <Image style={{position:'relative', top:0, left:0}} src={'/assets/accents/finger-print.png'} preview={false}/>
                 </div>
-                <div className={'w-2/5'} style={{backgroundColor: '#E6E9ED'}}>
-                    <div className={'pl-11 pr-10'}>
+                <div className={'w-full lg:w-2/5'} style={{backgroundColor: '#E6E9ED'}}>
+                    <div className={'px-5 lg:px-11'}>
                         <Form
                             layout={"vertical"}
-                            className={'pt-28'}
+                            className={'pt-12 lg:pt-28'}
                             form={form}
                         >
                             <Row gutter={16}>
-                                <Col span={12}>
+                                <Col xs={{span: 24}} sm={{span: 12}}>
                                     <Form.Item
-                                        label={'First Name'}
+                                        label={<label className={'text-gray-500 font-normal text-lg'}>First Name</label>}
                                         name={'firstname'}
                                         rules={[
                                             {
-                                                required: true,
+                                                required: false,
                                                 message:'You must input First Name'
                                             },
                                         ]}>
                                         <Input className={'border-transparent h-10'} style={{backgroundColor: '#B4BCC9'}}/>
                                     </Form.Item>
                                 </Col>
-                                <Col span={12}>
+                                <Col xs={{span: 24}} sm={{span: 12}}>
                                     <Form.Item
-                                        label={'Last Name'}
+                                        label={<label className={'text-gray-500 font-normal text-lg'}>Last Name</label>}
                                         name={'lastname'}
                                         rules={[
                                             {
-                                                required: true,
+                                                required: false,
                                                 message:'You must input Last Name'
                                             },
                                         ]}>
@@ -86,43 +88,43 @@ const ContactPage = () => {
                                 </Col>
                             </Row>
                             <Form.Item
-                                label={'Company'}
+                                label={<label className={'text-gray-500 font-normal text-lg'}>Company</label>}
                                 name={'company'}
                                 rules={[
                                     {
-                                       required: true,
+                                       required: false,
                                        message:'You must input your company'
                                     },
                                 ]}>
                                 <Input className={'border-transparent h-10'} style={{backgroundColor: '#B4BCC9'}}/>
                             </Form.Item>
                             <Form.Item
-                                label={'Email (Company’s Email Only)'}
+                                label={<label className={'text-gray-500 font-normal text-lg'}>Email (Company’s Email Only)</label>}
                                 name={'email'}
                                 rules={[
                                     {
-                                        required: true,
+                                        required: false,
                                         message:'You must input email company'
                                     },
                                 ]}>
                                 <Input className={'border-transparent h-10'} style={{backgroundColor: '#B4BCC9'}}/>
                             </Form.Item>
                             <Form.Item
-                                label={'Phone Number'}
+                                label={<label className={'text-gray-500 font-normal text-lg'}>Phone Number</label>}
                                 name={'phone_number'}
                                 rules={[
                                     {
-                                        required: true,
+                                        required: false,
                                         message:'You must input phone number'
                                     },
                                 ]}>
                                 <Input type={"number"} className={'border-transparent h-10 w-full validation'} style={{backgroundColor: '#B4BCC9'}}/>
                             </Form.Item>
                             <Form.Item
-                                label={'How Can We Help?'}
+                                label={<label className={'text-gray-500 font-normal text-lg'}>How Can We Help?</label>}
                                 rules={[
                                     {
-                                        required: true,
+                                        required: false,
                                         message:'You must input Last Name'
                                     },
                                 ]}>
