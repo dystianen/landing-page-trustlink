599c75c4dbbc6b7b957d544c00ae25e7ac985526
Tsabit Ghazwan <tsabitg23@gmail.com>
2022-02-16 01:23:38 +0700

    fix: typo and change address

--- pages/about.js
+++ pages/about.js
@@ -40,7 +40,7 @@ const AboutPage = () => {
                                         <p className="font-bold text-lg xl:text-xl">The Most Advance <br />
                                             <span className='font-light'> End-To-End</span><br />
                                             Digital Biometrics <br />
-                                            Solution Provider</p>
+                                            Solution Provider and Open Finance</p>
                                     </div>
                                 </Fade>
                             </div>
@@ -50,7 +50,7 @@ const AboutPage = () => {
                                 <div className={'h-full lg:h-3/5 w-full bg-[#D8D8D8]'}>
                                     <Slide when={transition} duration={1600} left>
                                         <div className="bg-[#D8D8D8] h-full">
-                                            <iframe className={'h-full'} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.3334781061417!2d106.82982881468574!3d-6.219681595497624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3f7062b8557%3A0xeab1cb9306fa3551!2sThe%20H%20Tower!5e0!3m2!1sen!2sid!4v1644525588163!5m2!1sen!2sid" width="100%" height="100%" loading="lazy" />
+                                            <iframe className={'h-full'} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.298439614077!2d106.83960811476904!3d-6.224324195494293!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f38cd5f00001%3A0x5cb013567aa7df01!2sKota%20Kasablanca%20Office%2088!5e0!3m2!1sid!2sid!4v1644949204792!5m2!1sid!2sid" width="100%" height="100%" loading="lazy" />
                                         </div>
                                     </Slide>
                                 </div>
