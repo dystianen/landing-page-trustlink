f46032528f847678df90c532e7ebf02e1d5c1500
Tsabit Ghazwan <tsabitg23@gmail.com>
2022-03-14 16:37:19 +0700

    fix: homepage plexus

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import {isAuthUrl} from "../utils/checkUrl";
 import {useStore} from "../components/StoreProvider";
 import {Image} from "antd";
 import React from "react";
+import {ParticlePage} from "../components/Particle";
 
 export const checkRerouteLoggedUser = (store, router)=>{
   if(typeof window !== 'undefined') {
@@ -24,6 +25,27 @@ export default function Home() {
           <Image preview={false} className="fixed w-14 top-4" src={'/assets/logo/icon-only.png'}/>
           <Image preview={false} className="relative h-14 left-14" src={'/assets/logo/text-only.png'}/>
         </div>
+        <section className={'pl-14'}>
+          <div className={'home-next-generation mt-30'}>
+              <span>Next</span>
+              <span>― Generation</span>
+          </div>
+            <div className={'home-next-generation-sub'}>
+              <span>Digital biometric</span>
+              <span style={{marginTop:-20}}>Identification system</span>
+            </div>
+        </section>
+          <div>
+              <ParticlePage
+                dimention={{
+                    width: '50%',
+                    height:'100vh'
+                }}
+                numberValue={240}
+                opacity={0.8}
+                className={'home-plexus'}
+              />
+          </div>
       </div>
   )
 }
