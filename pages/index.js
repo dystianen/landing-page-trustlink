b3b8ffe0a9452234114bd6b88d7295085aab763b
Tsabit Ghazwan <tsabitg23@gmail.com>
2022-03-14 15:05:38 +0700

    feat: homepage

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import {useRouter} from "next/router";
 import {appConfig} from "../config/app";
 import {isAuthUrl} from "../utils/checkUrl";
 import {useStore} from "../components/StoreProvider";
+import {Image} from "antd";
+import React from "react";
 
 export const checkRerouteLoggedUser = (store, router)=>{
   if(typeof window !== 'undefined') {
@@ -13,10 +15,15 @@ export default function Home() {
   const router = useRouter();
   const store = useStore();
 
-  checkRerouteLoggedUser(store,router);
+  // checkRerouteLoggedUser(store,router);
 
 
   return (
-      <div></div>
+      <div style={{padding: 16}}>
+        <div>
+          <Image preview={false} className="fixed w-14 top-4" src={'/assets/logo/icon-only.png'}/>
+          <Image preview={false} className="relative h-14 left-14" src={'/assets/logo/text-only.png'}/>
+        </div>
+      </div>
   )
 }
