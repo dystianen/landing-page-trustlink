4dfa28d631ce58a5594e69c0adae5c1ab94e2a7e
arissetyawan1 <aris.setyawan354313@gmail.com>
2022-03-15 18:32:37 +0700

    fix : text always reliable

--- pages/trusted_by.js
+++ pages/trusted_by.js
@@ -6,15 +6,22 @@ const TrustedBy = () => {
         <div className={'flex flex-row justify-between items-center w-full min-h-screen'}>
             {/* text section */}
             <div className={' w-full '}>
-                <span className={'text-[0.9em] tracking-[.19em] font-bold uppercase text-gray-500 text-opacity-30'} style={{fontFamily:'montserrat'}}>Always Reliable</span>
-                <span className={'we-are-text'}>Trusted By.</span>
-                <span style={{ fontSize: '3.4em', fontFamily: 'montserrat', fontWeight: '500', color:'#04204D', textTransform:'capitalize'}}>Clients & Partners</span>
+                <div className={'block'}>
+                    <div className={'flex flex-row items-center'}>
+                        <hr style={{ width: '3em', border:'solid grey '}} className={ 'inline-block mr-2 opacity-25'}/>
+                         <span className={'text-[0.9em] tracking-[.19em] font-bold uppercase text-gray-500 text-opacity-25'} style={{fontFamily:'montserrat'}}>Always Reliable</span>
+                    </div>
+                    <span className={'we-are-text'}>Trusted By.</span>
+                    <span style={{ fontSize: '3.4em', fontFamily: 'montserrat', fontWeight: '500', color:'#04204D', textTransform:'capitalize'}}>Clients & Partners</span>
+                </div>
+            {/* logos */}
+                <div className={'flex justify-end'}>
+                    <div className={'bg-gray-300 w-5/6'}>
+                        <p> Logos</p>
+                    </div>
+                  </div>
             </div>
 
-            {/* logos */}
-            <div className={'bg-red-600 w-3/4 absolute  '}>
-                <p> Logos</p>
-            </div>
         </div> 
     )
 }
