be43e6af1b74f38d9d8647e96f9ad0e96c811696
buildkr <dd3788973@gmail.com>
2022-03-16 17:32:31 +0700

    update: asset banner

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -9,7 +9,7 @@ const Banner = () => {
                 <div className="w-72 px-10 py-4 text-lg text-[#161D24] md:px-20 md:w-full md:py-4 md:text-3xl lg:px-20 lg:py-16 lg:text-3xl 2xl:text-5xl">
                     We are thrilled to advance our technology to meet the needs of our valued customers
                 </div>
-                <Image preview={false} className="w-96 h-full md:w-full lg:h-max" src={"../assets/banner1.png"}/>
+                <Image preview={false} className="w-96 h-full md:w-full lg:h-max" src={"../assets/banner_use.png"}/>
             </div>
     )
 }
