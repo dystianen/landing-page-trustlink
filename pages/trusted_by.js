e7af6b7f143ae34b2aa9a064a3858afe745b20c6
arissetyawan1 <aris.setyawan354313@gmail.com>
2022-03-16 13:24:28 +0700

    fix : content trusted by

--- pages/trusted_by.js
+++ pages/trusted_by.js
@@ -5,21 +5,22 @@ import React from "react";
 const TrustedBy = () => {
     return (
         <div className={'flex flex-row justify-between items-center w-full min-h-screen'}>
+            <div className={'h-[255px] w-5/6 bg-gray-200 z-10 absolute right-0 sm:top-44  ' }/>
             {/* text section */}
-            <div className={' w-full '}>
-                <div className={'block'}>
-                    <div className={'flex flex-row items-center'}>
-                        <hr style={{ width: '4.5em', border:'0', borderTop:'solid rgba(156, 163, 175) 3px'}}  className={ 'inline-block  opacity-30 mr-5 '}/>
+            <div className={' w-full z-50 '}>
+                <div className={'block z-50'}>
+                    <div className={'flex flex-row items-center z-50'}>
+                        <hr style={{ width: '4em', border:'0', borderTop:'solid rgba(156, 163, 175) 3px'}}  className={ 'inline-block  opacity-30 mr-3 '}/>
                          <span className={'text-[1em] tracking-[.23em] font-bold uppercase text-gray-400 text-opacity-30'} style={{fontFamily:'montserrat'}}>Always Reliable</span>
                     </div>
                     <span className={'we-are-text'}>Trusted By.</span>
                     <span style={{ fontSize: '3.4em', fontFamily: 'montserrat', fontWeight: '500', color:'#04204D', textTransform:'capitalize'}}>Clients & Partners</span>
                 </div>
                 {/* logos */}
-                <div className={'flex flex-row justify-end  mt-5'}>
+                <div className={'flex flex-row justify-end  mt-5 z-50'}>
                     <div className={"bg-gray-300  w-[95%]"}>
                         <div className={'grid grid-cols-5 divide-x-[2px] items-center bg-white text-center w-full'}>
-                            <div className={'flex justify-center items-center h-20'}>
+                            <div className={'flex justify-center items-center h-20 '}>
                                 <Image src={'assets/logo/adie-logo.png'} preview={false} width={85} />
                             </div>
                             <div className={'flex justify-center items-center h-20'}>
