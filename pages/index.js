94aa6f1fbe31c4296affde537f72ce0d22d78841
Dinda Ragil <dindawardani36@gmail.com>
2022-03-21 17:02:21 +0700

    fix: scroll-down asset

--- pages/index.js
+++ pages/index.js
@@ -46,141 +46,143 @@ export default function Home() {
 
 
   return (
-      <div className={'overflow-hidden'}>
-    <div>
-      <DrawerSlide menu={menu} isOpen={isOpen} />
-      <div className={'fixed w-20 h-20 transparent top-2/4 left-0 p-5'} style={{ zIndex: 10000 }}>
-        <button
-          className="flex flex-col w-full h-full justify-center group"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <div className={`${genericHamburgerLine} self-start ${isOpen
-            ? "w-full rotate-45 translate-y-3 opacity-100"
-            : "w-4 opacity-100"
-            }`}
-          />
-          <div className={`${genericHamburgerLine2} ${isOpen ? "opacity-0" : "opacity-100"
-            }`}
-          />
-          <div className={`${genericHamburgerLine} self-end ${isOpen
-            ? "w-full -rotate-45 -translate-y-3 opacity-100"
-            : "w-4 opacity-100"
-            }`}
-          />
-        </button>
-      </div>
-      <div className={'flex flex-row'} style={{ justifyContent: 'space-between' }}>
-        <div>
-          <Image preview={false} className="fixed w-14 top-4" src={'/assets/logo/icon-only.png'} style={{ zIndex: 10000 }} />
-          <Image preview={false} className="relative h-14 left-14" src={'/assets/logo/text-only.png'} />
-        </div>
-        <div className={'choose-lang flex flex-row mr-32 mt-10'}>
-          <p className={'cursor-pointer font-medium'}>EN</p>
-          <div className={'w-px h-5 mt-1 mx-2'} style={{ backgroundColor: '#818FA6' }} />
-          <p className={'cursor-pointer font-medium'}>ID</p>
-        </div>
-      </div>
-      <div className={'grid grid-cols-2'}>
-      <section className={'flex justify-start'}>
-          <div className={'flex items-end justify-center w-1/12'}>
-            <p className={'w-max -rotate-180'} style={{ color: '#161D24', fontFamily: 'Montserrat', letterSpacing: '0.15rem', writingMode: 'vertical-lr', textOrientation: 'sideways' }}>SCROLL DOWN</p>
-          </div>
-          <div className={'w-max ml-10'}>
-          <div className={'home-next-generation mt-30'}>
-            <span>Next</span>
-            <span className={'-mt-8'}>― Generation</span>
-          </div>
-          <div className={'home-next-generation-sub'}>
-            <span>Digital biometric</span>
-            <span style={{ marginTop: -20 }}>Identification system.</span>
-          </div>
-          </div>
-        </section>
-        <section>
-          <div style={{
-            backgroundColor: '#FFFFFF',
-            borderStyle: 'solid',
-            borderWidth: 8,
-            borderColor: '#FE7519',
-            boxShadow: '0px 12px 12px 9px #ff66001a',
-            borderRadius: '50% / 50%',
-            padding: 15,
-          }}
-            className={'w-36 h-36 flex justify-center items-center shadow-xl'}>
-            <Image preview={false} src={'/assets/logo/trust-scan/trust-scan.png'} className={'z-10 justify-center'} />
-          </div>
-          <div style={{
-            backgroundColor: '#FFFFFF',
-            padding: 20,
-            borderStyle: 'solid',
-            borderWidth: 8,
-            borderColor: '#FE7519',
-            boxShadow: '0px 12px 12px 9px #ff66001a',
-            borderRadius: '50% / 50%',
-            marginTop: '-17%',
-            marginLeft: '40%',
-          }}
-            className={'w-52 h-52 flex justify-center items-center shadow-xl'}>
-            <Image preview={false} src={'/assets/logo/trust-connect/trust-connect.png'} className={'z-10 justify-center'} />
-          </div>
-          <div style={{
-            backgroundColor: '#FFFFFF',
-            padding: 15,
-            borderStyle: 'solid',
-            borderWidth: 8,
-            borderColor: '#FE7519',
-            boxShadow: '0px 12px 12px 9px #ff66001a',
-            borderRadius: '50% / 50%',
-            marginTop: '17%',
-            marginLeft: '65%',
-          }}
-            className={'w-36 h-36 flex justify-center items-center shadow-xl'}>
-            <Image preview={false} src={'/assets/logo/trust-live/trust-live.png'} className={'z-10 justify-center'} />
-          </div>
-          <div style={{
-            backgroundColor: '#FFFFFF',
-            padding: 25,
-            borderStyle: 'solid',
-            borderWidth: 8,
-            borderColor: '#FE7519',
-            boxShadow: '0px 12px 12px 9px #ff66001a',
-            borderRadius: '50% / 50%',
-            marginTop: '-10%',
-            marginLeft: '7%',
-          }}
-            className={'w-52 h-52 flex justify-center items-center shadow-xl'}>
-            <Image preview={false} src={'/assets/logo/trust-verify/trust-verify.png'} className={'z-10 justify-center'} />
-          </div>
-        </section>
-      </div>
+    <div className={'overflow-hidden'}>
       <div>
-        <ParticlePage
-          dimention={{
-            width: '50%',
-            height: '100vh'
-          }}
-          numberValue={240}
-          opacity={0.8}
-          className={'home-plexus'}
-        />
+        <DrawerSlide menu={menu} isOpen={isOpen} />
+        <div className={'fixed w-20 h-20 transparent top-2/4 left-0 p-5'} style={{ zIndex: 10000 }}>
+          <button
+            className="flex flex-col w-full h-full justify-center group"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <div className={`${genericHamburgerLine} self-start ${isOpen
+              ? "w-full rotate-45 translate-y-3 opacity-100"
+              : "w-4 opacity-100"
+              }`}
+            />
+            <div className={`${genericHamburgerLine2} ${isOpen ? "opacity-0" : "opacity-100"
+              }`}
+            />
+            <div className={`${genericHamburgerLine} self-end ${isOpen
+              ? "w-full -rotate-45 -translate-y-3 opacity-100"
+              : "w-4 opacity-100"
+              }`}
+            />
+          </button>
+        </div>
+        <div className={'flex flex-row'} style={{ justifyContent: 'space-between' }}>
+          <div>
+            <Image preview={false} className="fixed w-14 top-4" src={'/assets/logo/icon-only.png'} style={{ zIndex: 10000 }} />
+            <Image preview={false} className="relative h-14 left-14 mt-4" src={'/assets/logo/text-only.png'} />
+          </div>
+          <div className={'choose-lang flex flex-row mr-32 mt-10'}>
+            <p className={'cursor-pointer font-medium'}>EN</p>
+            <div className={'w-px h-5 mt-1 mx-2'} style={{ backgroundColor: '#818FA6' }} />
+            <p className={'cursor-pointer font-medium'}>ID</p>
+          </div>
+        </div>
+        <div className={'grid grid-cols-2'}>
+          <section className={'flex justify-start'}>
+            <div className={'flex items-end justify-center'} style={{ width: '12%' }}>
+              <div className="auto-rows-auto"><p className={'w-max -rotate-180'} style={{ color: '#161D24', fontFamily: 'Montserrat', letterSpacing: '0.15rem', fontSize: '0.6rem', writingMode: 'vertical-lr', textOrientation: 'sideways' }}>SCROLL DOWN</p>
+                <Image preview={false} src={'/assets/scroll-down.svg'} />
+              </div>
+            </div>
+            <div className={'w-max ml-10'}>
+              <div className={'home-next-generation mt-30'}>
+                <span>Next</span>
+                <span className={'-mt-8'}>― Generation</span>
+              </div>
+              <div className={'home-next-generation-sub'}>
+                <span>Digital biometric</span>
+                <span style={{ marginTop: -20 }}>Identification system.</span>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div style={{
+              backgroundColor: '#FFFFFF',
+              borderStyle: 'solid',
+              borderWidth: 8,
+              borderColor: '#FE7519',
+              boxShadow: '0px 12px 12px 9px #ff66001a',
+              borderRadius: '50% / 50%',
+              padding: 15,
+            }}
+              className={'w-36 h-36 flex justify-center items-center shadow-xl'}>
+              <Image preview={false} src={'/assets/logo/trust-scan/trust-scan.png'} className={'z-10 justify-center'} />
+            </div>
+            <div style={{
+              backgroundColor: '#FFFFFF',
+              padding: 20,
+              borderStyle: 'solid',
+              borderWidth: 8,
+              borderColor: '#FE7519',
+              boxShadow: '0px 12px 12px 9px #ff66001a',
+              borderRadius: '50% / 50%',
+              marginTop: '-17%',
+              marginLeft: '40%',
+            }}
+              className={'w-52 h-52 flex justify-center items-center shadow-xl'}>
+              <Image preview={false} src={'/assets/logo/trust-connect/trust-connect.png'} className={'z-10 justify-center'} />
+            </div>
+            <div style={{
+              backgroundColor: '#FFFFFF',
+              padding: 15,
+              borderStyle: 'solid',
+              borderWidth: 8,
+              borderColor: '#FE7519',
+              boxShadow: '0px 12px 12px 9px #ff66001a',
+              borderRadius: '50% / 50%',
+              marginTop: '17%',
+              marginLeft: '65%',
+            }}
+              className={'w-36 h-36 flex justify-center items-center shadow-xl'}>
+              <Image preview={false} src={'/assets/logo/trust-live/trust-live.png'} className={'z-10 justify-center'} />
+            </div>
+            <div style={{
+              backgroundColor: '#FFFFFF',
+              padding: 25,
+              borderStyle: 'solid',
+              borderWidth: 8,
+              borderColor: '#FE7519',
+              boxShadow: '0px 12px 12px 9px #ff66001a',
+              borderRadius: '50% / 50%',
+              marginTop: '-10%',
+              marginLeft: '7%',
+            }}
+              className={'w-52 h-52 flex justify-center items-center shadow-xl'}>
+              <Image preview={false} src={'/assets/logo/trust-verify/trust-verify.png'} className={'z-10 justify-center'} />
+            </div>
+          </section>
+        </div>
+        <div>
+          <ParticlePage
+            dimention={{
+              width: '50%',
+              height: '100vh'
+            }}
+            numberValue={240}
+            opacity={0.8}
+            className={'home-plexus'}
+          />
+        </div>
+        <section className={"AboutMe"}>
+          <AboutMePage />
+        </section>
+        <section className={'pl-20 mt-52'}>
+          <Product />
+        </section>
       </div>
-      <section className={"AboutMe"}>
-        <AboutMePage />
-      </section>
-      <section className={'pl-20 mt-52'}>
-        <Product />
-      </section>
+      <div className="h-quarter" />
+      <UseCase />
+      <Banner />
+      <TrustedBy />
+      <WeAimed />
+      <div className="h-quarter" />
+      <CertificationMembership />
+      <div className="h-quarter" />
+      <Intouch />
+      <Footer />
     </div>
-    <div className="h-quarter" />
-    <UseCase />
-    <Banner />
-    <TrustedBy />
-    <WeAimed />
-    <div className="h-quarter" />
-    <CertificationMembership />
-    <div className="h-quarter" />
-    <Intouch />
-    <Footer />
-  </div>
   )
 }
