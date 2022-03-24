4d60edcf07211f5be83714bd90cd7a0a00d9d167
arissetyawan1 <aris.setyawan354313@gmail.com>
2022-03-24 12:04:47 +0700

    fix : trusted by

--- components/TrustedBy/index.js
+++ components/TrustedBy/index.js
@@ -3,8 +3,9 @@ import { propertyOf } from "lodash";
 import React from "react";
 import {useMediaQuery} from 'react-responsive'
 import { ParticlePage } from "../Particle";
-import Particles from "react-tsparticles";
+// import Particles from "react-tsparticles";
 import { ParticlesTrusted } from "./ParticlesTrusted";
+import { Particles } from "react-particles-js";
 
 const TrustedBy = () => {
       const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1224px)'})
