87fbeb1065d3e23ffe5a11d08207be3e39b86a95
Irvan Maulana <irvanmaulana0605@gmail.com>
2022-03-25 14:58:09 +0700

    fix: drawer size

--- pages/index.js
+++ pages/index.js
@@ -49,14 +49,14 @@ export default function Home() {
     <div className={'overflow-hidden'}>
       <div>
         <DrawerSlide menu={menu} isOpen={isOpen} />
-        <div className={'fixed w-20 h-20 transparent top-2/4 left-0 p-5'} style={{ zIndex: 10000 }}>
+        <div className={'fixed w-16 sm:w-20 h-20 transparent top-2/4 left-0 p-5'} style={{ zIndex: 10000 }}>
           <button
             className="flex flex-col w-full h-full justify-center group"
             onClick={() => setIsOpen(!isOpen)}
           >
             <div className={`${genericHamburgerLine} self-start ${isOpen
               ? "w-full rotate-45 translate-y-3 opacity-100"
-              : "w-4 opacity-100"
+              : "w-3 sm:w-4 opacity-100"
               }`}
             />
             <div className={`${genericHamburgerLine2} ${isOpen ? "opacity-0" : "opacity-100"
@@ -64,7 +64,7 @@ export default function Home() {
             />
             <div className={`${genericHamburgerLine} self-end ${isOpen
               ? "w-full -rotate-45 -translate-y-3 opacity-100"
-              : "w-4 opacity-100"
+              : "w-3 sm:w-4 opacity-100"
               }`}
             />
           </button>
