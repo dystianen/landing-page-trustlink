6b9a550a2e606fd70a7082bf0e7bfb520f8a4944
Irvan Maulana <irvanmaulana0605@gmail.com>
2022-03-28 14:02:14 +0700

    update: banner

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -1,40 +1,26 @@
 import React from "react";
-import {Col, Image, Row} from "antd";
+import {Image} from "antd";
 const Banner = () => {
     return (
-            // <div className="bg-[#FBFBFB] h-auto flex flex-row ml-20">
-            //     <div className="w-72 pl-12 py-4 text-lg text-[#161D24] md:px-20 md:w-full md:py-4 md:text-3xl lg:px-20 lg:py-16 lg:text-3xl 2xl:text-5xl">
-            //         We are thrilled to advance our technology to meet the needs of our valued customers
-            //     </div>
-            //     <Image preview={false} className="w-96 h-full md:w-full lg:h-max" src={"/assets/banner_bg.png"}/>
-            // </div>
-
-        // <Row className="ml-20 text-white h-auto bg-[#FBFBFB] text-[#161D24]">
-        //     <Col span={7} order={1}>
-        //       <Image preview={false} className="w-screen h-full" src={"/assets/banner_bg.svg"}/>
-        //     </Col>
-        //     <Col span={12} order={2}>
-        //       <div className="py-4 text-sm text-[#161D24] md:px-10 md:text-lg lg:px-20 lg:text-xl 2xl:text-3xl">
-        //             We are thrilled to advance our technology to meet the needs of our valued customers
-        //       </div>
-        //     </Col>
-        //     <Col span={5} order={3}>
-        //       {/*<Image preview={false} className="w-96 h-full md:w-full lg:h-max" src={"/assets/icon_trust.png"}/>*/}
-        //       <Image preview={false} className="w-min h-min pt-14 xs:pt-8" src={"/assets/icon_trust.png"}/>
-        //     </Col>
-        // </Row>
-    <Row className=" ml-20 text-white h-auto bg-[#FBFBFB] text-[#161D24]">
-            <Col className="grid justify-self-center" span={12} order={1}>
-                  {/*<Image preview={false} className="static w-screen h-full" src={"/assets/banner_bg.svg"}/>*/}
-                  {/*<div className="bg-banner bg-no-repeat bg-cover h-auto break-words py-8 px-4 text-lg text-[#161D24] md:h-64 md:px-10 md:text-xl lg:text-2xl 2xl:text-3xl">*/}
-                  <div className="bg-banner bg-no-repeat bg-cover h-auto monsterrat break-words text-lg text-[#161D24] px-8 py-12 md:h-64 md:text-xl lg:text-2xl 2xl:text-4xl">
-                        We are thrilled to advance our technology to meet the needs of our valued customers
-                  </div>
-            </Col>
-            <Col className="grid content-start mobilel:content-center mobilel:justify-items-center " span={12} order={3}>
-              <Image preview={false} className="w-min h-min" src={"/assets/icon_trust.png"}/>
-            </Col>
-        </Row>
+        <div className={'h-auto w-full pl-12 md:pl-20'}>
+            <div className="relative text-white h-[400px] sm:h-[450px] w-full bg-white">
+                <div className="absolute left-0 top-0 w-full h-full about-img">
+                    <Image preview={false} className="relative w-full h-[105%] invert-[.1] z-0" src={"/assets/images/accent-master-logo-3-crop.png"}/>
+                </div>
+                <div className={'flex flex-col md:flex-row w-full h-full'}>
+                    <div className="w-full md:w-3/5 h-full flex justify-center items-center z-10">
+                        <div className={'md:w-[80%] lg:w-auto'}>
+                            <p className="montserrat text-[#161D24] text-xl sm:text-2xl lg:text-3xl xl:text-[32px] font-medium mb-0 text-center md:text-left">
+                                We are thrilled to advance <br/>our technology to meet<br/> the needs of our valued customers
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-2/5 h-full flex justify-center items-center z-10">
+                        <Image preview={false} className={'h-[89px] w-auto'} src={"/assets/logo/logo-for-light-background.png"}/>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
