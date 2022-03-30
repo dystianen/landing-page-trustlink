af5a20e34c73cb6f1a3834ebab4817481a551475
Nur Azizah <nahanizalba9@gmail.com>
2022-03-30 23:11:31 +0700

    fix: reusable div for usecase

--- components/DrawerSlide/usecases.js
+++ components/DrawerSlide/usecases.js
@@ -6,32 +6,32 @@ import {LeftOutlined, RightOutlined} from "@ant-design/icons";
 export const UseCases = observer(({show, setShow}) => {
     const dataUseCases = [
         {
-            key: '',
-            title: '',
-            description: '',
-            bottom: '',
-            top: '',
+            key: 'sclaw',
+            title: 'Security and Laws.',
+            description: 'New technology to accommodate the future of security with TrustVision and TrustVerify.',
+            left: '/assets/logo/trust/vision.png',
+            right: '/assets/logo/trust/verify.png',
         },
         {
-            key: '',
-            title: '',
-            description: '',
-            bottom: '',
-            top: '',
+            key: 'bank',
+            title: 'Your Bank.',
+            description: 'With TrustConnect and TrustScore, you can manage your banking easier.',
+            left: '/assets/logo/trust/connect.png',
+            right: '/assets/logo/trust/score.png',
         },
         {
-            key: '',
-            title: '',
-            description: '',
-            bottom: '',
-            top: '',
+            key: 'user',
+            title: 'Interactive Scanning.',
+            description: 'To our user, we can trust TrustLive and TrustScan for your scanning purpose.',
+            left: '/assets/logo/trust/scan.png',
+            right: '/assets/logo/trust/live.png',
         },
         {
-            key: '',
-            title: '',
-            description: '',
-            bottom: '',
-            top: '',
+            key: 'data',
+            title: 'Manage User Data',
+            description: 'You can now scan your identity and verify it to goverment just by using our TrustVerify and TrustScan.',
+            left: '/assets/logo/trust/verify.png',
+            right: '/assets/logo/trust/scan.png',
         },
     ]
     return (
@@ -57,54 +57,22 @@ export const UseCases = observer(({show, setShow}) => {
                 </div>
             </div>
             <div className={'grid grid-cols-1 md:grid-cols-2 gap-0 mb-12 montserrat'}>
-                <div className={'w-full h-auto md:h-48 lg:h-52 border border-l-0 border-b-0 border-gray-300'}>
-                    <div className={'relative h-full flex flex-col justify-center items-center pl-2 lg:pl-8 pt-6'}>
-                        <p className={'md:text-base lg:text-xl text-[#1D365F] leading-tight w-[70%] mb-4 z-10'}>Security and Laws.</p>
-                        <p className={'montserrat md:text-[10px] lg:text-xs text-[#818FA6] w-[70%] mb-5 md:mb-0 z-10'}>New technology to accommodate the future of security with TrustVision and TrustVerify.</p>
-                        <div className={'absolute -bottom-2 left-0 z-0'}>
-                            <Image preview={false} className="w-[50px] md:w-[60px] lg:w-[80px]" src={'/assets/logo/trust/vision.png'}/>
-                        </div>
-                        <div className={'absolute top-0 right-0 z-0'}>
-                            <Image preview={false} className="w-[50px] md:w-[60px] lg:w-[80px]" src={'/assets/logo/trust/verify.png'}/>
-                        </div>
-                    </div>
-                </div>
-                <div className={'w-full h-auto md:h-48 lg:h-52 border border-l-0 border-b-0 border-gray-300'}>
-                    <div className={'relative h-full flex flex-col justify-center items-center pl-2 lg:pl-6 pt-6'}>
-                        <p className={'montserrat md:text-base lg:text-xl text-[#1D365F] leading-tight w-[70%] mb-4 z-10'}>Your Bank.</p>
-                        <p className={'montserrat md:text-[10px] lg:text-xs text-[#818FA6] w-[70%] mb-5 md:mb-0 z-10'}>With TrustConnect and TrustScore, you can manage your banking easier.</p>
-                        <div className={'absolute top-0 right-0 z-0'}>
-                            <Image preview={false} className="w-[50px] md:w-[60px] lg:w-[80px]" src={'/assets/logo/trust/connect.png'}/>
-                        </div>
-                        <div className={'absolute -bottom-2 left-0 z-0'}>
-                            <Image preview={false} className="w-[50px] md:w-[60px] lg:w-[80px]" src={'/assets/logo/trust/score.png'}/>
+                {dataUseCases.map((data, index) => {
+                    return (
+                    <div key={data.key} className={`w-full h-auto md:h-48 lg:h-52 border border-l-0 ${index >= 3 ? null : 'border-b-0'} border-gray-300`}>
+                        <div className={'relative h-full flex flex-col justify-center items-center pl-2 lg:pl-8 pt-6'}>
+                            <p className={'md:text-base lg:text-xl text-[#1D365F] leading-tight w-[70%] mb-4 z-10'}>{data.title}</p>
+                            <p className={'montserrat md:text-[10px] lg:text-xs text-[#818FA6] w-[70%] mb-5 md:mb-0 z-10'}>{data.description}</p>
+                            <div className={'absolute -bottom-2 left-0 z-0'}>
+                                <Image preview={false} className="w-[50px] md:w-[60px] lg:w-[80px]" src={data.left}/>
+                            </div>
+                            <div className={'absolute top-0 right-0 z-0'}>
+                                <Image preview={false} className="w-[50px] md:w-[60px] lg:w-[80px]" src={data.right}/>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className={'w-full h-auto md:h-48 lg:h-52 border border-l-0 border-gray-300'}>
-                    <div className={'relative h-full flex flex-col justify-center items-center pl-2 lg:pl-6 pt-6'}>
-                        <p className={'montserrat md:text-base lg:text-xl text-[#1D365F] leading-tight w-[70%] mb-4 z-10'}>We Know You.</p>
-                        <p className={'montserrat md:text-[10px] lg:text-xs text-[#818FA6] w-[70%] mb-5 md:mb-0 z-10'}>To our user, we can trust TrustLive and TrustScan for your scanning purpose.</p>
-                        <div className={'absolute top-0 right-0 z-0'}>
-                            <Image preview={false} className="w-[50px] md:w-[60px] lg:w-[80px]" src={'/assets/logo/trust/scan.png'}/>
-                        </div>
-                        <div className={'absolute -bottom-2 left-0 z-0'}>
-                            <Image preview={false} className="w-[50px] md:w-[60px] lg:w-[80px]" src={'/assets/logo/trust/live.png'}/>
-                        </div>
-                    </div>
-                </div>
-                <div className={'w-full h-auto md:h-48 lg:h-52 border border-l-0 border-gray-300'}>
-                    <div className={'relative h-full flex flex-col justify-center items-center pl-4 lg:pl-6 pt-6'}>
-                        <p className={'montserrat md:text-base lg:text-xl text-[#1D365F] leading-tight w-[90%] mb-4 z-10'}>Manage User Data.</p>
-                        <p className={'montserrat md:text-[10px] lg:text-xs text-[#818FA6] w-[90%] mb-5 md:mb-0 z-10'}>You can now scan your identity and verify it to goverment just by using our TrustVerify and TrustScan.</p>
-                        <div className={'absolute top-0 right-0 z-0'}>
-                            <Image preview={false} className="w-[50px] md:w-[60px] lg:w-[80px]" src={'/assets/logo/trust/verify.png'}/>
-                        </div>
-                        <div className={'absolute -bottom-2 left-0 z-0'}>
-                            <Image preview={false} className="w-[50px] md:w-[60px] lg:w-[80px]" src={'/assets/logo/trust/scan.png'}/>
-                        </div>
-                    </div>
-                </div>
+                    );
+                })}
             </div>
         </div>
     </div>
