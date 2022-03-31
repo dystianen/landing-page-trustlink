7866348663e76b56f32c3bcdabef2c21075f8775
Irvan Maulana <irvanmaulana0605@gmail.com>
2022-03-31 16:17:47 +0700

    fix: drawer when close

--- components/DrawerSlide/usecases.js
+++ components/DrawerSlide/usecases.js
@@ -39,16 +39,16 @@ export const UseCases = observer(({show, setShow}) => {
     return (
         <div className={'flex flex-row bg-white h-auto md:h-full min-h-[100vh]'}>
         <div className={'ml-20 border-l border-gray-300 h-auto md:h-full'}/>
-        <div className={'border-r border-gray-300 px-5 hidden md:inline-block'} onClick={() => setShow(!show)}>
-            <div className={'mt-16 w-12 h-12 rounded-full border-[1px] border-[#5B6D8A] flex justify-center items-center opacity-50'}>
+        <div className={'border-r border-gray-300 px-5 hidden md:inline-block'}>
+            <div className={'cursor-pointer mt-16 w-12 h-12 rounded-full border-[1px] border-[#5B6D8A] flex justify-center items-center opacity-50'} onClick={() => setShow(!show)}>
                 <LeftOutlined className={'text-xl mb-1 font-bold'}/>
             </div>
         </div>
         <div className={'w-full h-full'}>
             <div className={'w-full h-auto mt-4 md:mt-16 px-2 lg:px-5'}>
                 <div>
-                    <div className={'inline-block md:hidden mr-4 mb-4'} onClick={() => setShow(!show)}>
-                        <div className={'w-12 h-12 rounded-full border-[1px] border-[#5B6D8A] flex justify-center items-center opacity-50'}>
+                    <div className={'inline-block md:hidden mr-4 mb-4'}>
+                        <div className={'cursor-pointer w-12 h-12 rounded-full border-[1px] border-[#5B6D8A] flex justify-center items-center opacity-50'} onClick={() => setShow(!show)}>
                             <LeftOutlined className={'text-xl mb-1 font-bold'}/>
                         </div>
                     </div>
