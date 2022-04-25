ffb3c3bc0b1d729116f22774bb0442543a745b6a
Irvan Maulana <irvanmaulana0605@gmail.com>
2022-04-25 13:18:12 +0700

    fix: responsive product and overflow when click product on top

--- components/Product/index.js
+++ components/Product/index.js
@@ -25,7 +25,7 @@ export const Product = ({onClickContact=emptyFunction, productRef, indexProduct,
     const shadow = xl ? 20 : lg ? 10 : md ? 15: sm ? 15 : 5
     const step = xs ? 'w-10 h-10' : 'w-14 h-14'
     const stepActive = xs ? 'w-[50px] h-[50px]' : 'w-[65px] h-[65px]'
-    const imageSm = xs ? 'w-[16px]' : 'w-[24px]'
+    const imageSm = xs ? 'w-[16px]' : 'w-[18px]'
 
     const [isSafari, setIsSafari] = useState(false)
     useEffect(() => {
@@ -121,7 +121,7 @@ export const Product = ({onClickContact=emptyFunction, productRef, indexProduct,
 
     const Item = ({item}) => {
         return (
-            <div className={'flex flex-col md:flex-row w-full h-auto md:min-h-[28rem] lg:min-h-[38rem] xl:min-h-[42rem]'}>
+            <div className={'flex flex-col md:flex-row w-full min-h-[43rem] md:h-[28rem] lg:h-[38rem] xl:h-[42rem]'}>
                 <div className={`flex justify-center md:justify-end items-center w-full md:w-3/6 lg:w-2/4 text-center ${isSafari ? 'z-30' : 'z-10'}`}>
                     <ReactPlayer
                         className=''
@@ -135,11 +135,11 @@ export const Product = ({onClickContact=emptyFunction, productRef, indexProduct,
                 </div>
                 <div className={'flex flex-col justify-center mt-4 w-full md:w-3/6 lg:w-2/4 pl-12 md:pl-0 z-40'}>
                     <div>
-                        <p className={'text-3xl md:text-4xl lg:text-6xl mb-0 montserrat text-gray-400 font-bold leading-none'} style={{ color: item.coming_soon ? '#cbd5e0' : '#04204D' }}>{item.name}<span className={`${item.coming_soon ?  'text-gray-400' : 'text-orange' } font-medium`}>{item.name2} </span><span style={{ color: '#fe6601', position:'absolute'}} className={'text-lg md:text-xl'}>{item.coming_soon ? 'Coming Soon' : ''}</span></p>
+                        <p className={'text-center md:text-left text-3xl md:text-4xl lg:text-6xl mb-0 montserrat text-gray-400 font-bold leading-none'} style={{ color: item.coming_soon ? '#cbd5e0' : '#04204D' }}>{item.name}<span className={`${item.coming_soon ?  'text-gray-400' : 'text-orange' } font-medium`}>{item.name2} </span><span style={{ color: '#fe6601', position:'absolute'}} className={'text-lg md:text-xl'}>{item.coming_soon ? 'Coming Soon' : ''}</span></p>
                     </div>
-                    <p className={'w-[95%] md:w-[80%] text-xl md:text-2xl mb-0 montserrat leading-none font-bold mt-5'} style={{color: '#04204D'}}>{item.subhead}</p>
+                    <p className={'w-[95%] md:w-[80%] text-lg md:text-xl lg:text-2xl mb-0 montserrat leading-none font-bold mt-5 text-center md:text-left'} style={{color: '#04204D'}}>{item.subhead}</p>
                     <div className={'mt-5 md:mt-5'}>
-                        <p style={{color: '#04204D'}} className={"w-[95%] md:w-[80%] text-base lg:text-lg montserrat text-justify mb-3"}>
+                        <p style={{color: '#04204D'}} className={"w-[95%] md:w-[80%] text-sm md:text-base lg:text-lg montserrat text-justify mb-3"}>
                             {item.desc}
                         </p>
                         <a
@@ -168,7 +168,7 @@ export const Product = ({onClickContact=emptyFunction, productRef, indexProduct,
             <div className={'flex w-full justify-center'}>
                 <div className={'flex flex-row items-center z-10 mb-2'}>
                     <div className={'w-10 sm:w-16 h-[2px] bg-[#818FA6] opacity-50'}/>
-                    <p className={'text-sm mx-2 sm:mx-6 text-[#818FA6] montserrat mb-0 text-center'}>{t('One and For All')}</p>
+                        <p className={'text-sm mx-2 sm:mx-6 text-[#818FA6] montserrat mb-0 text-center'}>{t('One and For All')}</p>
                     <div className={'w-10 sm:w-16 h-[2px] bg-[#818FA6] opacity-50'}/>
                 </div>
             </div>
@@ -178,62 +178,65 @@ export const Product = ({onClickContact=emptyFunction, productRef, indexProduct,
                     {/*<p className={'text-[26px] sm:text-3xl md:text-4xl lg:text-5xl text-center mt-2 text-[#161D24] montserrat font-medium'}>{t('End to End Services')}</p>*/}
                     <br />
                     <div className={'text-[18px] leading-relaxed text-center text-[#4F6382] montserrat w-full flex justify-center items-center'}>
-                        <p className='sm:w-4/6 lg:w-[45%] xl:w-[35%]'>{t('We are a leading digital biometric')}</p>
+                        <p className='z-10 sm:w-4/6 lg:w-[45%] xl:w-[35%]'>{t('We are a leading digital biometric')}</p>
                     </div>
                 </div>
             </div>
-            <div className={'relative w-screen lg:w-auto'} ref={productRef}>
-                <Steps responsive={false} current={current} className={'mt-6 md:mt-1 w-full h-32 sm:h-48 -ml-12 md:-ml-20 lg:ml-0 mr-8'} onChange={(num) => onChangeProduct(num)}>
-                    {data.map((it, index) => (
-                        <Step
-                            className={'flex justify-center items-center'}
-                            key={index}
-                            icon={
-                                <div className={`flex flex-col justify-content items-center ${current === index ? 'mt-2 lg:mt-0' : 'mt-12 sm:mt-10 lg:mt-12'}`}>
-                                    <div className={`rounded-full border-4 ${current === index ? `${stepActive} sm:w-[78px] md:w-[95px] lg:w-32 sm:h-[78px] md:h-[95px] lg:h-32 border-[#FE6601]` : `${step} sm:w-16 md:w-20 lg:w-24 sm:h-16 md:h-20 lg:h-24 border-[#FE944D]`} mb-2 bg-white z-10 flex justify-center items-center`}
-                                         style={{transition: 'width 200ms, height 200ms', boxShadow: current === index ? `0 0 ${shadow}px 1px #FE6601` : null}}
-                                    >
-                                        <Tooltip title={'Trust' +it.name}>
-                                            <Image preview={false} src={it.icon} className={`${imageSm} sm:w-[28px] md:w-[35px] lg:w-[50px]`}/>
-                                        </Tooltip>
+            <div ref={productRef}>
+                <div className={'relative w-screen lg:w-auto'}>
+                    <Steps responsive={false} current={current} className={'mt-6 md:mt-1 w-full h-32 sm:h-48 -ml-12 md:-ml-20 lg:ml-0 mr-8'} onChange={(num) => onChangeProduct(num)}>
+                        {data.map((it, index) => (
+                            <Step
+                                className={'flex justify-center items-center'}
+                                key={index}
+                                icon={
+                                    <div className={`flex flex-col justify-content items-center ${current === index ? 'mt-2 lg:mt-0' : 'mt-12 sm:mt-10 lg:mt-12'}`}>
+                                        <div className={`rounded-full border-4 ${current === index ? `w-[55px] h-[55px] sm:w-[78px] md:w-[95px] lg:w-32 sm:h-[78px] md:h-[95px] lg:h-32 border-[#FE6601]` : `w-12 h-12 sm:w-16 md:w-20 lg:w-24 sm:h-16 md:h-20 lg:h-24 border-[#FE944D]`} mb-2 bg-white z-10 flex justify-center items-center`}
+                                             style={{transition: 'width 200ms, height 200ms', boxShadow: current === index ? `0 0 ${shadow}px 1px #FE6601` : null}}
+                                        >
+                                            <Tooltip title={'Trust' +it.name}>
+                                                <Image preview={false} src={it.icon} className={`w-[18px] sm:w-[28px] md:w-[35px] lg:w-[50px]`}/>
+                                            </Tooltip>
+                                        </div>
+                                        <p className={`w-24 md:w-40 text-[10px] sm:text-sm lg:text-lg montserrat text-[#B4BCC9] ${current === index ? 'hidden' : null}`}><span className={'font-bold hidden sm:contents'}>Trust</span>{it.name}</p>
                                     </div>
-                                    <p className={`w-24 md:w-40 text-[10px] sm:text-sm lg:text-lg montserrat text-[#B4BCC9] ${current === index ? 'hidden' : null}`}><span className={'font-bold hidden sm:contents'}>Trust</span>{it.name}</p>
-                                </div>
+                                }
+                            />
+                        ))}
+                        <hr className={`absolute right-0 top-[48%] ${xs ? 'w-[105%]' : 'w-full' } lg:w-[90%] h-1 bg-[#FE6601] z-0`}/>
+                    </Steps>
+                </div>
+
+                <div className={'mt-4 md:mt-8 z-10'}>
+                    <Carousel
+                        navButtonsAlwaysVisible={true}
+                        animation={'slide'}
+                        next={num => onChangeProduct(num)}
+                        prev={num => onChangeProduct(num)}
+                        index={current}
+                        autoPlay={false}
+                        NextIcon={<Image preview={false} className="w-[20px] lg:w-[25px] h-[20px] lg:h-[25px]" src={'/assets/arrow-right.svg'}/>}
+                        PrevIcon={<Image preview={false} className="w-[20px] lg:w-[25px] h-[20px] lg:h-[25px]" src={'/assets/arrow-left.svg'}/>}
+                        indicators={false}
+                        navButtonsProps={{
+                            style: {
+                                backgroundColor: '#000000',
+                                opacity: 0.15,
                             }
-                        />
-                    ))}
-                    <hr className={`absolute right-0 top-[48%] ${xs ? 'w-[105%]' : 'w-full' } lg:w-[90%] h-1 bg-[#FE6601] z-0`}/>
-                </Steps>
+                        }}
+                        navButtonsWrapperProps={{
+                            className: `sm:mr-5 sm:ml-5 ${isSafari ? 'z-40' : 'z-40'} ${!current ? 'arrow-prev-product' : current === (items.length - 1) ? 'arrow-next-product' : null}`
+                        }}
+                        className={'-ml-12 md:ml-0'}
+                    >
+                        {
+                            items.map( (item, i) => <Item key={i} item={item} /> )
+                        }
+                    </Carousel>
+                </div>
             </div>
 
-            <div className={'mt-4 md:mt-8 z-10'}>
-                <Carousel
-                    navButtonsAlwaysVisible={true}
-                    animation={'slide'}
-                    next={num => onChangeProduct(num)}
-                    prev={num => onChangeProduct(num)}
-                    index={current}
-                    autoPlay={false}
-                    NextIcon={<Image preview={false} className="w-[20px] lg:w-[25px] h-[20px] lg:h-[25px]" src={'/assets/arrow-right.svg'}/>}
-                    PrevIcon={<Image preview={false} className="w-[20px] lg:w-[25px] h-[20px] lg:h-[25px]" src={'/assets/arrow-left.svg'}/>}
-                    indicators={false}
-                    navButtonsProps={{
-                        style: {
-                            backgroundColor: '#000000',
-                            opacity: 0.15,
-                        }
-                    }}
-                    navButtonsWrapperProps={{
-                        className: `mr-5 ml-5 ${isSafari ? 'z-40' : 'z-10'} ${!current ? 'arrow-prev-product' : current == 5 ? 'arrow-next-product' : null}`
-                    }}
-                    className={'-ml-12 md:ml-0'}
-                >
-                    {
-                        items.map( (item, i) => <Item key={i} item={item} /> )
-                    }
-                </Carousel>
-            </div>
-            <div className="relative h-[10vh]" />
+            <div className="relative h-[3vh] sm:h-[10vh]" />
             <div className={`absolute w-full lg:w-10/12 xl:w-8/12 h-1/3 sm:h-auto -bottom-10 right-0 usecase-img ${isSafari ? 'z-30' : 'z-0'}`}>
                 <Image preview={false} src={'/assets/images/accent-home-sectionProduct-2-edit.png'} className={'h-full w-full opacity-10'}/>
             </div>
