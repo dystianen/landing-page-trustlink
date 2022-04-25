3dcc989dc36089915975d612a7df55852d9a9391
Bintang Dhiya <bintang.dhiya@gmail.com>
2022-04-25 17:32:25 +0700

    fix: react particles randomly changing speed

--- components/Particle/index.js
+++ components/Particle/index.js
@@ -82,7 +82,7 @@ export const ParticlePage = (props) => {
                             width: 1,
                         },
                         collisions: {
-                            enable: true,
+                            enable: false
                         },
                         move: {
                             direction: "none",
