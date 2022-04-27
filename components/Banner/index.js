4e3e3eaff32c1e5fe1da69522b39dc1801d84d81
Irvan Maulana <irvanmaulana0605@gmail.com>
2022-04-27 13:30:48 +0700

    fix: banner text width

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -11,12 +11,12 @@ const Banner = () => {
                 </div>
                 <div className={'flex flex-col md:flex-row w-full h-full'}>
                     <div className="w-full md:w-3/5 h-full flex justify-center items-center z-10">
-                        <div className={'md:w-[80%] lg:w-3/4'}>
-                            <p className="montserrat text-[#161D24] text-xl sm:text-2xl lg:text-3xl xl:text-[24px] font-medium mb-0 text-center md:text-left">
+                        <div className={'md:w-[28rem] lg:w-[36rem] xl:w-[42rem] ml-4'}>
+                            <p className="montserrat text-[#161D24] text-lg lg:text-xl xl:text-2xl font-medium mb-0 ">
                                 {/*<span className={'xl:text-[26px] font-bold'}>*/}
                                 {/*    {t('BannerText1')}*/}
                                 {/*</span>*/}
-                                <br/>{t('BannerText2')}
+                                {t('BannerText2')}
                             </p>
                         </div>
                     </div>
