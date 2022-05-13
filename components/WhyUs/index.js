0d0bca94a46393670426efda0e0b1796d9e2cbaa
Bintang Dhiya <bintang.dhiya@gmail.com>
2022-05-13 20:55:38 +0700

    add: Why TrustLink page

---
+++ components/WhyUs/index.js
@@ -0,0 +1,71 @@
+import { CloseOutlined } from '@ant-design/icons';
+import {Image, Avatar, Divider, Typography} from 'antd';
+import React, {useEffect, useState} from 'react';
+import Fade from "react-reveal/Fade";
+import Slide from 'react-reveal/Slide';
+import { useRouter } from 'next/router';
+import { useTranslation } from 'next-i18next';
+
+const { Title, Text } = Typography;
+
+const WhyUsPage = () => {
+    const router = useRouter();
+    const { t } = useTranslation('common');
+    console.log(t, 'isi')
+    const [transition, setTransition] = useState(false);
+
+    useEffect(() => {
+        setTransition(true);
+    }, []);
+
+    const dataWhyUs = [
+        {
+            title: "Fast and Easy Integration",
+            description: "With our API based delivery, Your Business can provide the service in no time, and no hassle",
+            image_url: "/assets/why_us/API_laptop.png"
+        },{
+            title: "Proven State Of The Art Technology",
+            description: "Our services such as biometric facial recognition, Liveness, and OCR all has international licence such as NIST and iBETA",
+            image_url: "/assets/why_us/fingerprint_laptop.png"
+        },{
+            title: "Fast and Easy Integration",
+            description: "With our API based delivery, Your Business can provide the service in no time, and no hassle",
+            image_url: "/assets/why_us/fingerprint_laptop.png"
+        },{
+            title: "Fast and Easy Integration",
+            description: "With our API based delivery, Your Business can provide the service in no time, and no hassle",
+            image_url: "/assets/why_us/profile_laptop.png"
+        },
+    ]
+
+    return (
+        <div className='relative md:pl-20 pl-12 h-auto mt-30 mb-30 overflow-x-none flex flex-col'>
+            <div className={"absolute -top-30 left-[70%] w-[50rem] lg:w-[67rem] h-auto"}>
+                <Image src={"/assets/why_us/bg1.png"} preview={false}/>
+            </div>
+            <div>
+                <div className={'flex flex-row items-center mb-2'}>
+                    <div className={'w-16 h-[2px] mr-6 opacity-50'} style={{backgroundColor: '#818FA6'}}/>
+                    <p className={'text-xs mb-0 text-center text-[#818FA6] tracking-widest'}>{t('Why TrustLink')}</p>
+                </div>
+                <div>
+                    <p className={"text-2xl sm:text-4xl lg:text-5xl xl:text-7xl lg:w-[70%] sm:mb-10 font-[montserrat] font-bold sm:font-semibold text-[#1D365F]"}>We Create The Best Result-Oriented Offerings</p>
+                </div>
+            </div>
+            <div className={"flex flex-col sm:grid sm:grid-cols-2 gap-4 sm:pl-10 justify-items-center items-center"}>
+                {dataWhyUs.map((item, index) =>(
+                    <div className={"flex flex-col items-center max-w-[19rem] lg:max-w-[24rem] xl:max-w-[30rem] mb-10 sm:mb-0"}>
+                        <Image src={item.image_url} />
+                        <Divider />
+                        <div>
+                            <p className={'font-[montserrat] text-sm font-bold text-center mb-1'}>{item.title}</p>
+                            <p className={'font-[montserrat] text-xs text-center'}>{item.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default WhyUsPage;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { appConfig } from "../config/app";
 import { isAuthUrl } from "../utils/checkUrl";
 import { useStore } from "../components/StoreProvider";
-import { Button, Image } from "antd";
+import { Image } from "antd";
 import React, { useRef, useState } from "react";
 import { ParticlePage } from "../components/Particle";
 import { DrawerSlide } from "../components/DrawerSlide";
@@ -21,6 +21,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import Link from "next/link";
 import StickyHeader from "../components/StickyHeader";
 import {MapAddress} from "../components/MapAddress";
+import WhyUsPage from "../components/WhyUs";
 
 export const checkRerouteLoggedUser = (store, router) => {
     if (typeof window !== 'undefined') {
@@ -64,19 +65,19 @@ export default function Home() {
     const product = [
         {
             image: 'trust-scan.png',
-            className: 'cursor-pointer w-20 h-20 lg:w-24 lg:h-24 xl:w-32 xl:h-32 2xl:w-40 2xl:h-40 flex justify-center items-center shadow-xl bg-white border-[6px] xl:border-8 border-[#FF6703] rounded-full p-3.5 relative -ml-4 md:ml-0 lg:ml-16 xl:ml-24 z-10',
+            className: 'cursor-pointer w-20 h-20 lg:w-24 lg:h-24 xl:w-32 xl:h-32 2xl:w-40 2xl:h-40 flex justify-center items-center shadow-xl bg-white border-[6px] xl:border-8 border-[#FF6600] rounded-full p-3.5 relative -ml-4 md:ml-0 lg:ml-16 xl:ml-24 z-10',
             productIndex: 1 // index carousel
         },{
             image: 'trust-verify.png',
-            className: 'cursor-pointer w-32 h-32 lg:w-36 lg:h-36 xl:w-44 xl:h-44 2xl:w-52 2xl:h-52 flex justify-center items-center shadow-xl bg-white border-[6px] xl:border-8 border-[#FF6703] rounded-full p-5 relative z-10 -mt-12 lg:-mt-24 ml-[30%] md:ml-[40%]',
+            className: 'cursor-pointer w-32 h-32 lg:w-36 lg:h-36 xl:w-44 xl:h-44 2xl:w-52 2xl:h-52 flex justify-center items-center shadow-xl bg-white border-[6px] xl:border-8 border-[#FF6600] rounded-full p-5 relative z-10 -mt-12 lg:-mt-24 ml-[30%] md:ml-[40%]',
             productIndex: 0 // index carousel
         },{
             image: 'trust-live.png',
-            className: 'cursor-pointer w-20 h-20 lg:w-24 lg:h-24 xl:w-32 xl:h-32 2xl:w-40 2xl:h-40 flex justify-center items-center shadow-xl bg-white border-[6px] xl:border-8 border-[#FF6703] rounded-full p-3.5 absolute bottom-10 md:bottom-[30vh] z-10 ml-[65%] md:ml-[65%]',
+            className: 'cursor-pointer w-20 h-20 lg:w-24 lg:h-24 xl:w-32 xl:h-32 2xl:w-40 2xl:h-40 flex justify-center items-center shadow-xl bg-white border-[6px] xl:border-8 border-[#FF6600] rounded-full p-3.5 absolute bottom-10 md:bottom-[30vh] z-10 ml-[65%] md:ml-[65%]',
             productIndex: 2 // index carousel
         },{
             image: 'trust-vision.png',
-            className: 'cursor-pointer w-32 h-32 lg:w-36 lg:h-36 xl:w-44 xl:h-44 2xl:w-52 2xl:h-52 flex justify-center items-center shadow-xl bg-white border-[6px] xl:border-8 border-[#FF6703] rounded-full p-5 absolute -bottom-6 md:bottom-28 2xl:bottom-36 z-10 ml-0 md:ml-[7%] lg:ml-[18%]',
+            className: 'cursor-pointer w-32 h-32 lg:w-36 lg:h-36 xl:w-44 xl:h-44 2xl:w-52 2xl:h-52 flex justify-center items-center shadow-xl bg-white border-[6px] xl:border-8 border-[#FF6600] rounded-full p-5 absolute -bottom-6 md:bottom-28 2xl:bottom-36 z-10 ml-0 md:ml-[7%] lg:ml-[18%]',
             productIndex: 3 // index carousel
         },
     ]
@@ -102,7 +103,7 @@ export default function Home() {
                 changeMenuOpen={setIsOpen}
                 isMenuOpen={isOpen}
                 onClickProduct={() => sectionProductRef.current.scrollIntoView({ behavior: 'smooth' })}
-                onClickContactUs={() => contactUsRef.current.scrollIntoView({ behavior: 'smooth' }) }
+                onClickMembership={() => membershipRef.current.scrollIntoView({ behavior: 'smooth' }) }
                 onClickClients={() => clientsRef.current.scrollIntoView({ behavior: 'smooth' }) }
                 onClickUseCases={() => useCasesRef.current.scrollIntoView({ behavior: 'smooth' }) }
                 onClickAboutUs={() => aboutUsRef.current.scrollIntoView({ behavior: 'smooth' }) }
@@ -133,18 +134,18 @@ export default function Home() {
                             </Link>
                         </div>
                     </div> */}
-                    <div className={'relative w-full pl-6 sm:pl-12'}>
+                    <div className={'relative w-full pl-12 sm:pl-20'}>
                         <section className={'flex justify-start w-auto h-full'}>
                             {/*<div className={'flex items-end justify-center w-32'}>*/}
                             {/*<div className={'auto-rows-auto invisible ml-8 lg:ml-3 xl:ml-0 md:visible'}><p className={'w-max -rotate-180'} style={{ color: '#161D24', fontFamily: 'Montserrat', letterSpacing: '0.15rem', fontSize: '0.6rem', writingMode: 'vertical-lr', textOrientation: 'sideways' }}>SCROLL DOWN</p>*/}
                             {/*    <Image preview={false} src={'/assets/scroll-down.svg'} />*/}
                             {/*</div>*/}
                             {/*</div>*/}
-                            <div className={'w-max h-screen lg:h-[40vh] md:h-full md:min-h-screen lg:pl-8 flex flex-col justify-start md:justify-center'}>
+                            <div className={'w-max h-[40vh] md:h-full md:min-h-screen lg:pl-8 flex flex-col justify-end md:justify-center'}>
                                 {/*<div className={'home-next-generation text-3xl sm:text-4xl md:text-5xl lg:text-5xl'}>*/}
                                 {/*    <span className={'mb-2'}><span className={''}>Trust-Centric Solution</span> For Identity Management  And Open Finance Platform</span>*/}
                                 {/*</div>*/}
-                                <div className={'md:mt-24 lg:mt-32 home-next-generation-sub text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-[3.25rem] font-medium z-10'}>
+                                <div className={'mt-24 lg:mt-32 home-next-generation-sub text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-[3.25rem] font-medium z-10'}>
                                     <span className={'block leading-tight'}>
                                         <span className={'font-bold block text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-5xl mb-4'}>{t('Trust Centric Solution')}</span>
                                         <span>{t('For Identity Management')}</span>
@@ -153,16 +154,11 @@ export default function Home() {
                                     <div>
                                         <p className={'mt-8 text-justify md:text-left w-[20rem] md:w-[36rem] lg:w-[42rem] text-sm md:text-xl lg:text-2xl tracking-normal font-normal'} style={{lineHeight: 1.75}}>{t('Desc Top Page')}</p>
                                     </div>
-                                    <div>
-                                        <Button className={"text-white text-sm lg:text-base xl:text-lg border-[#fe6601] bg-[#fe6601] montserrat rounded-lg h-12 w-36 md:w-48"} onClick={()=> aboutUsRef.current.scrollIntoView({ behavior: 'smooth' }) }>
-                                                Get it Started
-                                        </Button>
-                                    </div>
                                 </div>
 
                             </div>
                         </section>
-                        <section className={'absolute top-[17rem] md:top-20 right-10 md:-right-10 lg:-right-12 xl:-right-28 w-3/5 h-1/2 md:h-full mt-6 md:mt-0 mb-24 md:mb-0'}>
+                        <section className={'absolute top-[100%] md:top-20 right-10 md:-right-10 lg:-right-12 xl:-right-28 w-3/5 h-full mt-6 md:mt-0 mb-24 md:mb-0'}>
                             {
                                 product.map((it,index) => (
                                     <div key={index} className={it.className} onClick={()=> handleClickProduct(it.productIndex) }>
@@ -187,7 +183,7 @@ export default function Home() {
                 <section>
                     <AboutMePage  sectionRef={aboutUsRef}/>
                 </section>
-                <section className={'pl-6 sm:pl-12 md:pl-20 mt-52'}>
+                <section className={'pl-12 md:pl-20 mt-52'}>
                     <Product
                         onClickContact={()=>contactUsRef.current.scrollIntoView({behavior: 'smooth'}) }
                         productRef={sectionProductRef}
@@ -195,11 +191,14 @@ export default function Home() {
                         setIndexProduct={setIndex}
                     />
                 </section>
-                <UseCase sectionRef={useCasesRef} onClickContact={()=>contactUsRef.current.scrollIntoView({behavior: 'smooth'}) }/>
+
+                <WhyUsPage />
+
+                <UseCase sectionRef={useCasesRef} />
                 <Banner />
                 <TrustedBy sectionRef={clientsRef}/>
                 <WeAimed />
-                {/* <div className="h-[20vh]" /> */}
+                <div className="h-[20vh]" />
                 <CertificationMembership sectionRef={membershipRef}/>
                 <div className="h-quarter" />
                 <ContactUs sectionRef={contactUsRef } />
