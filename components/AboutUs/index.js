5a890367ada103c8409dd62e9f035092f19f9932
arissetyawan1 <aris.setyawan354313@gmail.com>
2022-05-13 18:22:47 +0700

    fix : centered align

--- components/AboutUs/index.js
+++ components/AboutUs/index.js
@@ -39,14 +39,19 @@ const AboutMePage = ({sectionRef}) => {
             <div className={'w-full mt-20 sm:mt-10 mr-10 flex justify-end section-most-top'} ref={sectionRef}>
                 <div className={'w-full sm:w-[100vw]'}>
                         <div className={'flex flex-col sm:pl-32 md:pl-12 lg:pl-24 xl:pl-40 2xl:pl-[15%] xl:pt-10 '}>
-                            <div className={'flex flex-row items-center mb-2'}>
+                            {/* <div className={'flex flex-row items-center mb-2'}>
                                 <div className={'w-16 h-[2px] mr-6 opacity-50'} style={{backgroundColor: '#818FA6'}}/>
                                 <p className={'text-sm mb-0 text-center text-[#818FA6] tracking-widest'}>{t('About Us.')}</p>
-                            </div>
+                            </div> */}
                             {/*<Fade when={transition} duration={1600}>*/}
-                            <div>
-                                <Title className={"font-bold w-full lg:w-[90%] xl:w-[90%] text-lg md:text-xl lg:text-2xl montserrat"} style={{whiteSpace: 'pre-line'}}>
-                                        <span className={'block leading-snug text-[#04204D]'}>{t('about_us_main_text')}</span>
+                            <div >
+                            <Title className={"font-bold w-full lg:w-[90%] xl:w-[90%] text-lg md:text-xl lg:text-2xl montserrat"} style={{ whiteSpace: 'pre-line' }}>
+                                        <div className={'flex flex-row justify-center items-center z-10  text-center mb-5'}>
+                                            <div className={'w-10 sm:w-16 h-[2px] bg-[#818FA6] opacity-50'}/>
+                                                <p className={'text-sm mx-2 sm:mx-6 text-[#818FA6] montserrat mb-0 text-center'}>{t('About Us.')}</p>
+                                            <div className={'w-10 sm:w-16 h-[2px] bg-[#818FA6] opacity-50'}/>
+                                        </div>
+                                        <span className={'block leading-snug text-[#04204D] text-center'}>{t('about_us_main_text')}</span>
                                     </Title>
                             </div>
                         </div>
