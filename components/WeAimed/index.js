225cb9616141bf86de49547a09c70c84d3433d19
arissetyawan1 <aris.setyawan354313@gmail.com>
2022-05-13 14:50:49 +0700

    rev : img

--- components/WeAimed/index.js
+++ components/WeAimed/index.js
@@ -1,17 +1,23 @@
-import {Image} from "antd";
+import { Image } from "antd";
+import { useMediaQuery } from 'react-responsive';
 import {useTranslation} from "next-i18next";
 
 const WeAimed = () => {
-
+    const tablet = useMediaQuery({ query: '(max-width: 980px)' });
     const { t } = useTranslation('common');
 
     return (<>
             <div className="h-[340px] w-full pl-12 md:pl-20 z-0 ">
                 <div className={'w-full h-full relative text-white montserrat '}>
-                    {/* <div className={'section-gradient-bg w-full h-full absolute z-10'} /> */}
-                    <div className={'absolute triv-top top-6 md:top-16 md:top-5 -left-12 sm:-left-20 h-[120%] md:h-[175%] sm:h-auto w-1/2 sm:w-auto about-img z-0'}>
-                        <Image preview={false} src={'/assets/images/accent-home-sectionTrivia-1.jpg'} className={'h-full w-auto'}/>
+                {/* <div className={'section-gradient-bg w-full h-full absolute z-10'} /> */}
+                {tablet === true ?
+                    <div className={'absolute triv-top top-6 md:top-16 md:top-5 -left-16 sm:-left-20 h-[120%] md:h-[175%] sm:h-auto w-1/2 sm:w-auto about-img z-0'}>
+                        {/* <Image preview={false} src={'/assets/images/accent-home-sectionTrivia-1.jpg'} className={'h-full w-auto'}/> */}
+                    </div> :
+                    <div className={'absolute triv-top top-6 md:top-16 md:top-5 -left-20 sm:-left-64 h-[120%] md:h-[175%] sm:h-auto w-1/2 sm:w-auto about-img z-0'}>
+                        <Image preview={false} src={'/assets/trivia.png'} className={'h-full w-auto'}/>
                     </div>
+                }                    
                     <div className={'absolute top-0 left-0 h-[70%] md:h-full w-auto about-img z-30'}>
                         {/* <Image preview={false} src={'/assets/images/accent-home-sectionTrivia-2.jpg'} className={'h-full w-auto opacity-40'}/> */}
                     </div>
