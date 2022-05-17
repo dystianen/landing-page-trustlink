984751c18015d69ce6f769864ca7438c63f3efbd
tsabitg23 <tsabitg23@gmail.com>
2022-05-18 03:33:11 +0700

    add: Why TrustLink page

--- components/WhyUs/index.js
+++ components/WhyUs/index.js
@@ -40,13 +40,13 @@ const WhyUsPage = () => {
 
     return (
         <div className='relative md:pl-20 pl-12 h-auto mt-30 mb-30 overflow-x-none flex flex-col'>
-            <div className={"absolute -top-30 left-[75%] w-[50rem] lg:w-[67rem] h-auto"}>
+            <div className={"absolute -top-52 lg:-top-30 left-[75%] w-[50rem] lg:w-[67rem] h-auto"}>
                 <Image src={"/assets/why_us/bg1.png"} preview={false}/>
             </div>
             <div>
                 <div className={'flex flex-row items-center mb-2'}>
                     <div className={'w-16 h-[2px] mr-6 opacity-50'} style={{backgroundColor: '#818FA6'}}/>
-                    <p className={'text-xs mb-0 text-center text-[#818FA6] tracking-widest montserrat'}>{t('Why TrustLink')}</p>
+                    <p className={'text-xs lg:text-base tracking-wide mb-0 text-center text-[#818FA6] tracking-widest montserrat'}>{t('Why TrustLink')}</p>
                 </div>
                 <div>
                     <p className={"text-2xl sm:text-4xl lg:text-5xl xl:text-7xl lg:w-[75%] sm:mb-10 montserrat font-bold sm:font-semibold text-[#1D365F]"}>We Create The Best Result-Oriented Offerings</p>
@@ -54,12 +54,12 @@ const WhyUsPage = () => {
             </div>
             <div className={"flex flex-col sm:grid sm:grid-cols-2 gap-4 sm:pl-10 justify-items-center items-center"}>
                 {dataWhyUs.map((item, index) =>(
-                    <div className={"flex flex-col items-center max-w-[19rem] lg:max-w-[24rem] xl:max-w-[30rem] mb-10 sm:mb-0"}>
-                        <Image src={item.image_url} />
+                    <div key={index} className={"box-hover py-5 px-[1.875rem] flex flex-col text-[#1D365F] items-center max-w-[19rem] lg:max-w-[30rem] xl:max-w-[40rem] mb-10 sm:mb-0"}>
+                        <Image src={item.image_url} preview={false} />
                         <Divider />
                         <div>
-                            <p className={'montserrat text-sm font-bold text-center mb-1'}>{item.title}</p>
-                            <p className={'montserrat text-xs text-center'}>{item.description}</p>
+                            <p className={'montserrat text-lg md:text-xl lg:text-2xl font-bold text-center mb-1'}>{item.title}</p>
+                            <p className={'montserrat text-sm md:text-base lg:text-lg text-center'}>{item.description}</p>
                         </div>
                     </div>
                 ))}
