78b66a9ca8a28a67821b05e1d20927e6d0a8b5ed
Irvan Maulana <irvanmaulana0605@gmail.com>
2022-05-23 17:31:52 +0700

    update: logo trustlink banner and address

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -12,7 +12,7 @@ const Banner = () => {
                 <div className={'flex flex-col md:flex-row w-full h-full'}>
                     <div className="w-full md:w-3/5 h-full flex justify-center items-center z-10">
                         <div className={'md:w-[28rem] lg:w-[36rem] xl:w-[42rem] ml-4'}>
-                            <p className="montserrat text-[#04204D] text-lg lg:text-xl xl:text-2xl font-medium mb-0 mt-5">
+                            <p className="montserrat text-[#04204D] text-lg lg:text-xl xl:text-2xl font-medium mb-0 mt-5 md:mt-0">
                                 <span className={'leading-10 text-2xl sm:text-3xl lg:text-5xl xl:text-6xl font-bold'}>
                                     {t('BannerText1')}
                                 </span>
@@ -22,7 +22,7 @@ const Banner = () => {
                         </div>
                     </div>
                     <div className="w-full md:w-2/5 h-full flex justify-center items-center z-10">
-                        <Image preview={false} className={'h-auto w-auto'} src={"/assets/logo/logo-for-light-background.png"}/>
+                        <Image preview={false} className={'h-auto w-full px-14'} src={"/assets/logo/logo-banner-fix.png"}/>
                     </div>
                 </div>
             </div>
