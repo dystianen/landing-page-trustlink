e2305772fd767f967624510c768f733c26744854
arissetyawan1 <aris.setyawan354313@gmail.com>
2022-05-24 11:15:24 +0700

    add : fav icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from "react";
-import '../styles/globals.scss';
-import '../styles/home.scss';
-import 'tailwindcss/tailwind.css';
-import { appWithTranslation } from 'next-i18next'
-import {StoreProvider} from "../components/StoreProvider";
-import Script from 'next/script'
+import React, { useEffect, useState } from "react";
+import "../styles/globals.scss";
+import "../styles/home.scss";
+import "tailwindcss/tailwind.css";
+import { appWithTranslation } from "next-i18next";
+import { StoreProvider } from "../components/StoreProvider";
+import Script from "next/script";
 import Head from "next/head";
-import {ModalLoader} from "../components/Modal/ModalLoader";
-import {ModalLoaderContext} from "../utils/modal";
+import { ModalLoader } from "../components/Modal/ModalLoader";
+import { ModalLoaderContext } from "../utils/modal";
 function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
@@ -19,26 +19,29 @@ function MyApp({ Component, pageProps }) {
     setText: setModalText,
   };
 
-
-
-  return <StoreProvider {...pageProps}>
-    <Head>
-      <title>Trustlink</title>
-      <meta name="viewport" content="width=device-width, minimum-scale=1.0"/>
-      {/*<link rel="shortcut icon" href={profile?.entity?.logo_url ? appConfig.imageApiUrl + profile?.entity?.logo_url : "/assets/logo.svg"} />*/}
-      {/*<link rel="shortcut icon" href="/assets/icons/map-marker.png" />*/}
-      <link rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Montserrat:bold,light,regular,medium"/>
-      <Script src={"../styles/animation/textCarousel.js"} />
-    </Head>
-    {getLayout(
+  return (
+    <StoreProvider {...pageProps}>
+      <Head>
+        <title>Trustlink</title>
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0" />
+        {/*<link rel="shortcut icon" href={profile?.entity?.logo_url ? appConfig.imageApiUrl + profile?.entity?.logo_url : "/assets/logo.svg"} />*/}
+        <link rel="icon" href="/assets/logo/icon-only.png" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Montserrat:bold,light,regular,medium"
+        />
+        <Script src={"../styles/animation/textCarousel.js"} />
+      </Head>
+      {getLayout(
         <>
-      <ModalLoader isOpen={modalLoading} text={modalText} />
+          <ModalLoader isOpen={modalLoading} text={modalText} />
           <ModalLoaderContext.Provider value={modalContextValue}>
-              <Component {...pageProps} />
+            <Component {...pageProps} />
           </ModalLoaderContext.Provider>
-    </>)}
-  </StoreProvider>;
+        </>
+      )}
+    </StoreProvider>
+  );
 }
 
-export default appWithTranslation(MyApp)
+export default appWithTranslation(MyApp);
