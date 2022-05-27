c742dfeae576f6f72238fae9647e0c2bd2f505e8
Bintang Dhiya <bintang.dhiya@gmail.com>
2022-05-27 17:20:21 +0700

    fix: drawerslide 'Email' capitalized + changed gap between the call us, email & address

--- components/DrawerSlide/index.js
+++ components/DrawerSlide/index.js
@@ -52,7 +52,7 @@ export const DrawerSlide = observer((props) => {
     const goToContact = () => {
         props.setOpen(false);
         onClickContact()
-        
+
     }
 
     const onMenuClick = (name) => {
@@ -113,20 +113,20 @@ export const DrawerSlide = observer((props) => {
                         <About show={showAbout} setShow={setShowAbout} />
                     </div>
                 </Fade>
-                <div className={`${(showProduct || showUseCase) ? 'w-auto md:w-[35%] md:fixed md:right-0 lg:py-8' : 'w-auto md:w-[60%] md:pl-14'}  md:py-16 pl-6 ml-16 sm:ml-20 md:ml-0 bg-[#1D365F] h-auto`} style={{ transition: 'width 500ms'}}>
+                <div className={`${(showProduct || showUseCase) ? 'w-auto md:w-[35%] md:fixed md:right-0 lg:py-8' : 'w-auto md:w-[60%] md:pl-14'} overflow-y-auto lg:overflow-y-hidden md:py-16 pl-6 ml-16 sm:ml-20 md:ml-0 bg-[#1D365F] h-max lg:h-auto`} style={{ transition: 'width 500ms'}}>
                     <div className={'flex flex-col w-full h-full justify-between'}>
                         <div className={`flex flex-col lg:${(showProduct || showUseCase) ? "flex-col lg:pt-0" : "flex-row justify-between"} pt-10 md:pt-20 h-full`} style={{ transition: 'width 300ms ease-in-out'}}>
                             <div className={`flex flex-col`}>
-                                <div className={'flex-col text-white mb-10 md:mb-2 -mt-5'}>
+                                <div className={'md:h-28 lg:h-40 flex-col text-white mb-10 md:mb-2'}>
                                     <p className={'text-xs opacity-80 mb-3 montserrat'} style={{letterSpacing: 1.8}}>{t('Call Us')}</p>
                                     <p className={`text-lg md:text-xl lg:text-xl xl:text-2xl mb-2 montserrat`}>(021) 29602116 / 29602117</p>
                                     <p className={'text-xs opacity-80 montserrat'} style={{letterSpacing: 0.3}}>{t('Mon to Fri')} (8:30 - 20:00)</p>
                                 </div>
-                                <div className={'flex-col text-white mb-10  md:mb-2 cursor-pointer'} onClick={goToContact}>
-                                    <p className={'text-xs opacity-80 mb-3 montserrat'} style={{letterSpacing: 1.8}}>Email</p>
+                                <div className={'md:h-28 lg:h-40 flex-col text-white mb-10  md:mb-2 cursor-pointer'} onClick={goToContact}>
+                                    <p className={'text-xs opacity-80 mb-3 montserrat'} style={{letterSpacing: 1.8}}>EMAIL</p>
                                     <a className={`text-xl ${(showProduct || showUseCase) ? 'md:text-xl' : 'md:text-2xl'} mb-2 montserrat text-white`}>info@withtrustlink.com</a>
                                 </div>
-                                <div className={'flex-col text-white mb-10  md:mb-2'}>
+                                <div className={'lg:h-40 flex-col text-white mb-10  md:mb-2'}>
                                     <p className={'text-xs opacity-80 mb-3 montserrat'} style={{letterSpacing: 1.8}}>{t('Address')}</p>
                                     {/*<p className={`text-xl ${(showProduct || showUseCase) ? 'md:text-xl' : 'md:text-2xl'} mb-2 montserrat`}>{t('Kota Kasablanca Office 88')}</p>*/}
                                     {/*<p className={'text-xs opacity-80 montserrat'} style={{letterSpacing: 0.3}}>{t('Jl. Raya Casablanca No.16, RW.5,')}<br/>{t(' Menteng Dalam, Kec. Tebet, DKI Jakarta 12870')}</p>*/}
@@ -135,7 +135,7 @@ export const DrawerSlide = observer((props) => {
                                 </div>
                             </div>
                             <div className={'flex-col text-white mb-10  md:mb-2'}>
-                                <p className={'text-xs opacity-80 mb-3 montserrat lg:mr-16'} style={{letterSpacing: 1.8}}>{t('Social Media')}</p>
+                                <p className={'text-xs opacity-80 mb-3 montserrat w-28 lg:mr-16'} style={{letterSpacing: 1.8}}>{t('Social Media')}</p>
                                 {socialMedia.map((data, index) => {
                                     return <p key={data.key} className={`text-xl ${(showProduct || showUseCase) ? 'md:text-xl' : 'md:text-2xl'} mb-2 montserrat`}>
                                         <a href={data.url} target={"_blank"} rel="noopener noreferrer" className="text-white">{data.title}</a>
@@ -145,7 +145,7 @@ export const DrawerSlide = observer((props) => {
 
                         </div>
                         <div className={'flex flex-row w-full justify-start'}>
-                            <div className={'w-full md:w-auto mb-12 md:mb-0'}>
+                            <div className={'w-full md:w-auto mb-12 md:mb-0 drawer-mt'}>
                                 <h1 className={`text-white text-xl  ${(showProduct || showUseCase) ? 'md:text-xl' : 'md:text-2xl'} font-bold mb-3 montserrat`}>
                                     {t('Advance Solutions')} <br/>
                                     <span className={'font-light'}>{t('For On Boarding Experience')}</span>
