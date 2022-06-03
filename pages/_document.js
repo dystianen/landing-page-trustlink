f4a440621d465339248957a0528cccf2065a5613
tsabitg23 <tsabitg23@gmail.com>
2022-06-03 15:19:35 +0700

    feat: add google analitys

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,25 @@
 import Document, {Head, Html, Main, NextScript} from 'next/document'
-
+import Script from 'next/script';
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
         return { ...initialProps }
     }
-
     render() {
         return (
             <Html>
+                <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+                <Script strategy="lazyOnload">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+                </Script>
                 <Head>
                     <meta name="viewport" content="width=device-width, minimum-scale=1.0" />
                     <link
