39e0978cd184c8b41364b589a0184250af074e2b
tsabitg23 <tsabitg23@gmail.com>
2022-06-14 16:34:49 +0700

    fix: add script

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, {Head, Html, Main, NextScript} from 'next/document'
-
+import Script from 'next/script';
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
@@ -9,6 +9,19 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
+                <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+                <Script strategy="lazyOnload">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+                </Script>
+
                 <Head>
                     <link
                         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
