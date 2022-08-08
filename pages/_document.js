6bfb4324d39e1e44ef05b1dd2190020639e7c05f
tsabitg23 <tsabitg23@gmail.com>
2022-08-08 13:52:12 +0700

    fix: add space

--- pages/_document.js
+++ pages/_document.js
@@ -8,18 +8,24 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+                {/* Global Site Tag (gtag.js) - Google Analytics */}
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                />
 
-                <Script strategy="lazyOnload">
-                    {`
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                     gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
+                      page_path: window.location.pathname,
                     });
-                `}
-                </Script>
+                  `,
+                    }}
+                />
                 <Head>
                     <meta name="viewport" content="width=device-width, minimum-scale=1.0" />
                     <link
